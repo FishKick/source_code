46f789348b49ba832e80ae4082fbce04a9feb112
FishKick <106138736+FishKick@users.noreply.github.com>
2022-09-14 01:07:47 +0700

    Update Sort.cpp

--- Sort.cpp
+++ Sort.cpp
@@ -27,6 +27,17 @@ void bubbleSort(int a[], int n){
 	}
 }
 
+void insertion_sort(int a[], int n){
+	for (int i = 1; i < n; i++){
+		int x = a[i]; int pos = i - 1;
+		while (pos >= 0 && x < a[pos]){
+			a[pos+1]  = a[pos];
+			pos--;
+		}
+		a[pos+1] = x;
+	}
+}
+
 
 int main(){
 	
