52704132af9ba1d525941acb14435bd425a73b39
FishKick <106138736+FishKick@users.noreply.github.com>
2022-09-22 17:17:12 +0700

    multiSet_unorderedSet

--- multiset.cpp
+++ multiset.cpp
@@ -14,34 +14,20 @@ using namespace std;
 //outP: 3 3 4 5 5 8 9 10
 
 int main(){
-//	multiset<int> ms;
-//	ms.insert(100);
-//	ms.insert(200);
-//	ms.insert(300);
-//	ms.insert(100);
-//	
-//	for (int x : ms)
-//	cout << x << ' ';// 100 100 200 300
-//	
-////	chú ý dùng erase
-//
-//	ms.erase(100); // xoa het 100 
-//	// 200 3000
-//	
-////	muốn xóa 1 phần tử ta nên dùng iterator
+
 	int n, k; cin >> n >> k;
 	int a[n];
 	multiset<int> ms;
 	for (int& x : a) cin >> x;
-//	for (int i = 0;i < k; i++){
-//		ms.insert(a[i]);	
-//	}
-//	for (int i = k; i < n; i++){
-//		cout << *ms.rbegin() << ' ';
-//		ms.erase(ms.find(a[i-k]	));
-//		ms.insert(a[i]);
-//	}
-//	cout << *ms.rbegin() << endl;
+	for (int i = 0;i < k; i++){
+		ms.insert(a[i]);	
+	}
+	for (int i = k; i < n; i++){
+		cout << *ms.rbegin() << ' ';
+		ms.erase(ms.find(a[i-k]	));
+		ms.insert(a[i]);
+	}
+	cout << *ms.rbegin() << endl;
 	
 	unordered_set<int> us;
 	for (int i = 0; i < n; i++){
